07b6dd3aec2d1d62a00c4aef91205b02c65e988b
IrenaMartty <irenamartincevic3@gmail.com>
2023-06-11 13:43:07 +0200

    agrego archivo js

--- Clase 1 - Introduccion JS -Variables -Tipos de datos -Prompt y alert/script.js
+++ Clase 1 - Introduccion JS -Variables -Tipos de datos -Prompt y alert/script.js
@@ -3,5 +3,39 @@ var nombre2 = 'Lore';
 
 let apellido = 'Ferrer';
 const mensaje = 'mensaje de prueba';
-
+console.log (apellido)
 apellido = 'Petriella'
+console.log (apellido)
+
+// TIPOS DE DATOS
+
+// datos primitivos
+// strig = cadenas de caracteres
+// number
+// boolean = true / false
+// undefined
+// null
+// NaN = not a number
+
+let uno;
+let dos = '1'
+
+console.log(uno)
+
+// 
+
+// let name = prompt('ingresa tu nombre', 'nombre')
+// let lastName = prompt('ingresa tu apellido', 'apellido')
+// let age = prompt('ingresa tu edad', 'edad')
+
+// console.log('nombre', name);
+// console.log('apellido', lastName);
+// console.log('edad', age);
+
+// alert(`Hola ${name}`)
+
+let primerNumero = prompt('ingrese un numero', '');
+let segundoNumero = prompt('ingrese un numero', '');
+let resultado = Number (primerNumero) + Number(segundoNumero);
+
+alert(`El resultado es ${resultado}`)
